edce677871ca8723969ab9703fb632ebc2326d20
Gilles Bruno <gilles_bruno@gmx.fr>
2022-09-08 11:58:46 +0200

    Comments about TypeORM decorator

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,12 @@ import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
 
+/**
+ * NB : option 'synchronize' = true means that every change in the entities 
+ * updates the data structure in the database
+ * Onlyuse in a dev environment.
+ * For production env., use migration files instead !!
+ */
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'sqlite',
