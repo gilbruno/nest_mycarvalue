8541bb287bfe7276e1a081e568c786c935498f10
Gilles Bruno <gilles_bruno@gmx.fr>
2022-09-08 16:06:14 +0200

    All fior CRUD API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Get, Delete,  Patch, Param, Query } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+
+    constructor(private usersService: UsersService) {
+
+
+    }
+
+    @Post('signup')
+    createUser(@Body() body: CreateUserDto) {
+        console.log(body)
+        this.usersService.create(body.email, body.password)
+    }    
+
+    @Get('/:id')
+    findUser(@Param('id') id:string) {
+        return this.usersService.findOne(parseInt(id))
+    }
+
+    @Get()
+    findAllUsers(@Query('email') email: string) {
+        return this.usersService.find(email)
+    }
+
+    @Delete('/:id')
+    removeUser(@Param('id') id:string) {
+        return this.usersService.remove(parseInt(id))
+    }
+}
