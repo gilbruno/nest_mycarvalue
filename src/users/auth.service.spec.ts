83b40f8e26c5c2207cfef643a5eeb101bde007b1
Gilles Bruno <gilles_bruno@gmx.fr>
2022-09-10 18:07:23 +0200

    Begin with Testing

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,29 @@
+import { Test } from "@nestjs/testing";
+import { AuthService } from "./auth.service";
+import { UsersService } from "./users.service";
+
+it('cancreate an instance of auth service', async () => {
+    //Creata a fake copy of users service
+    const fakeUsersService = {
+        find: () => {
+            Promise.resolve([])
+        },
+        create: (email:string, password,string) => {
+            Promise.resolve({id: 1, email, password})
+        }
+    }
+    //Create a DI container
+    const module = await Test.createTestingModule({
+        providers: [
+            AuthService,
+            {
+                provide: UsersService,
+                useValue: fakeUsersService
+            }
+        ]
+    }).compile()
+
+    const service = module.get(AuthService)
+
+    expect(service).toBeDefined()
+})
