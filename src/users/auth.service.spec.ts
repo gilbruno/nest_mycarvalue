54b2befca1f1d6cf4c23826629401363026eb669
Gilles Bruno <gilles_bruno@gmx.fr>
2022-09-11 18:57:33 +0200

    Add type to Mock

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,21 +1,22 @@
 import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
+import { User } from "./user.entity";
 import { UsersService } from "./users.service";
+
 
 it('cancreate an instance of auth service', async () => {
     //Creata a fake copy of users service
-    const fakeUsersService = {
-        find: () => {
-            Promise.resolve([])
-        },
-        create: (email:string, password,string) => {
-            Promise.resolve({id: 1, email, password})
-        }
+    const fakeUsersService: Partial<UsersService> = {
+        find: () => Promise.resolve([]),
+        create: (email:string, password: string) => 
+            Promise.resolve({id: 1, email, password} as User)
+        
     }
+    
     //Create a DI container
     const module = await Test.createTestingModule({
         providers: [
-            AuthService,
+            AuthService,    
             {
                 provide: UsersService,
                 useValue: fakeUsersService
