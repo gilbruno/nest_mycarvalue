72efcbf3135836e08a9e2b826f86162abd707a96
Gilles Bruno <gilles_bruno@gmx.fr>
2022-09-11 20:52:21 +0200

    Add new tests

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -4,27 +4,50 @@ import { User } from "./user.entity";
 import { UsersService } from "./users.service";
 
 
-it('cancreate an instance of auth service', async () => {
-    //Creata a fake copy of users service
-    const fakeUsersService: Partial<UsersService> = {
-        find: () => Promise.resolve([]),
-        create: (email:string, password: string) => 
-            Promise.resolve({id: 1, email, password} as User)
-        
-    }
-    
-    //Create a DI container
-    const module = await Test.createTestingModule({
-        providers: [
-            AuthService,    
-            {
-                provide: UsersService,
-                useValue: fakeUsersService
+describe(
+    'AuthService', () => {
+        let service: AuthService;
+
+        beforeEach(async () => {
+            //Creata a fake copy of users service
+            const fakeUsersService: Partial<UsersService> = {
+                find: () => Promise.resolve([]),
+                create: (email:string, password: string) => 
+                    Promise.resolve({id: 1, email, password} as User)
+                
             }
-        ]
-    }).compile()
+            
+            //Create a DI container
+            const module = await Test.createTestingModule({
+                providers: [
+                    AuthService,    
+                    {
+                        provide: UsersService,
+                        useValue: fakeUsersService
+                    }
+                ]
+            }).compile()
+        
+            service = module.get(AuthService)
+        
+        });
+        
+        it('can create an instance of auth service', async () => {
+            expect(service).toBeDefined()
+        })
+        
+        it('create a newuser with a salted and hash password', async () => {
+            const password_test = 'asdf'
+            const user = await service.signup('asf@asf.com', password_test)
 
-    const service = module.get(AuthService)
+            expect(user.password).not.toEqual(password_test)
 
-    expect(service).toBeDefined()
-})
+            const [salt, hash] = user.password.split('.')
+            expect(salt).toBeDefined()
+            expect(hash).toBeDefined()
+
+        })
+    }
+);
+
+
